e6f2891f12f1cf59a57d260958ffdc11f188b7c0
Marcello Kazuo <marcellokazuo@gmail.com>
2022-04-07 01:40:20 -0300

    app fix

--- react-native-cli/App.js
+++ react-native-cli/App.js
@@ -1,20 +1,9 @@
-import { StyleSheet, View } from 'react-native';
 import LoginScreen from './src/screens/LoginScreen';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <LoginScreen />
-    </View>
+    <LoginScreen />
   );
 }
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
