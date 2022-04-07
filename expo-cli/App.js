72566b4decf82c73789ca13fb30486f56bdd1639
Marcello Kazuo <marcellokazuo@gmail.com>
2022-04-07 00:53:44 -0300

    step-01 app.js simplificado

--- expo-cli/App.js
+++ expo-cli/App.js
@@ -1,19 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Screen 1</Text>
     </View>
   );
 }
 
+export default App;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
