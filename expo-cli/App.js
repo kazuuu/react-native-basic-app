9d2be07a41af971d41237a7c71953f0849d01ab2
Marcello Kazuo <marcellokazuo@gmail.com>
2022-04-07 10:28:08 -0300

    arrow function fix

--- expo-cli/App.js
+++ expo-cli/App.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native';
 
-const App = () => (
-  <View style={styles.container}>
-    <Text>Screen 1</Text>
-  </View>
-);
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Screen 1</Text>
+    </View>
+  );
+}
 
 export default App;
 
