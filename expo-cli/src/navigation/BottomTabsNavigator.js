1477bf011690d1f83eeafffb0ceca0e135732d2d
Marcello Kazuo <marcellokazuo@gmail.com>
2022-05-03 19:44:39 -0300

    Platform ios vs android

--- expo-cli/src/navigation/BottomTabsNavigator.js
+++ expo-cli/src/navigation/BottomTabsNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text} from 'react-native';
+import { Text, Platform } from 'react-native';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import HomeScreen from "../screens/HomeScreen"
@@ -10,91 +10,91 @@ export default () => {
     const BottomTab = createBottomTabNavigator();
 
     return (
-        <BottomTab.Navigator
-            screenOptions={({ route }) => ({
-                headerShown: false,
-                unmountOnBlur: true,
-                tabBarShowLabel: false,
-                tabBarStyle: { height: 50 },
-                tabBarActiveTintColor: 'tomato',
-                tabBarInactiveTintColor: 'gray',
-            })}
-        >
-            <BottomTab.Screen 
-                name="HomeTab" 
-                component={HomeScreen}
-                options={{
-                    tabBarIcon: ({ focused }) => (
-                        <>
-                            <MaterialCommunityIcons
-                                name='home'
-                                size={20}
-                                color={focused ? '#ee125a' : '#91A2AD'}
-                            />
-                            <Text
-                                allowFontScaling={false}
-                                style={{
-                                    color: focused ? '#ee125a' : '#91A2AD',
-                                    width: 50,
-                                    fontSize: 11,
-                                    textAlign: 'center'
-                                }}>
-                                Home
-                            </Text>
-                        </>
-                    )
-                }}            
-            />
-            <BottomTab.Screen
-                name="MessageTab"
-                component={MessageScreen} 
-                options={{
-                    tabBarIcon: ({ focused }) => (
-                        <>
-                            <MaterialCommunityIcons
-                                name='message'
-                                size={20}
-                                color={focused ? '#ee125a' : '#91A2AD'}
-                            />
-                            <Text
-                                allowFontScaling={false}
-                                style={{
-                                    color: focused ? '#ee125a' : '#91A2AD',
-                                    width: 50,
-                                    fontSize: 11,
-                                    textAlign: 'center'
-                                }}>
-                                Message
-                            </Text>
-                        </>
-                    )
-                }}
-            />
-            <BottomTab.Screen
-                name="AccountTab"
-                component={AccountScreen} 
-                options={{
-                    tabBarIcon: ({ focused }) => (
-                        <>
-                            <MaterialCommunityIcons
-                                name='account'
-                                size={20}
-                                color={focused ? '#ee125a' : '#91A2AD'}
-                            />
-                            <Text
-                                allowFontScaling={false}
-                                style={{
-                                    color: focused ? '#ee125a' : '#91A2AD',
-                                    width: 50,
-                                    fontSize: 11,
-                                    textAlign: 'center'
-                                }}>
-                                Account
-                            </Text>
-                        </>
-                    )
-                }}
-            />
-        </BottomTab.Navigator>
+            <BottomTab.Navigator
+                screenOptions={({ route }) => ({
+                    headerShown: false,
+                    unmountOnBlur: true,
+                    tabBarShowLabel: false,
+                    tabBarStyle: { height: Platform.OS === 'ios' ? 100 : 50 },
+                    tabBarActiveTintColor: 'tomato',
+                    tabBarInactiveTintColor: 'gray',
+                })}
+            >
+                <BottomTab.Screen 
+                    name="HomeTab" 
+                    component={HomeScreen}
+                    options={{
+                        tabBarIcon: ({ focused }) => (
+                            <>
+                                <MaterialCommunityIcons
+                                    name='home'
+                                    size={20}
+                                    color={focused ? '#ee125a' : '#91A2AD'}
+                                />
+                                <Text
+                                    allowFontScaling={false}
+                                    style={{
+                                        color: focused ? '#ee125a' : '#91A2AD',
+                                        width: 50,
+                                        fontSize: 11,
+                                        textAlign: 'center'
+                                    }}>
+                                    Home
+                                </Text>
+                            </>
+                        )
+                    }}            
+                />
+                <BottomTab.Screen
+                    name="MessageTab"
+                    component={MessageScreen} 
+                    options={{
+                        tabBarIcon: ({ focused }) => (
+                            <>
+                                <MaterialCommunityIcons
+                                    name='message'
+                                    size={20}
+                                    color={focused ? '#ee125a' : '#91A2AD'}
+                                />
+                                <Text
+                                    allowFontScaling={false}
+                                    style={{
+                                        color: focused ? '#ee125a' : '#91A2AD',
+                                        width: 50,
+                                        fontSize: 11,
+                                        textAlign: 'center'
+                                    }}>
+                                    Message
+                                </Text>
+                            </>
+                        )
+                    }}
+                />
+                <BottomTab.Screen
+                    name="AccountTab"
+                    component={AccountScreen} 
+                    options={{
+                        tabBarIcon: ({ focused }) => (
+                            <>
+                                <MaterialCommunityIcons
+                                    name='account'
+                                    size={20}
+                                    color={focused ? '#ee125a' : '#91A2AD'}
+                                />
+                                <Text
+                                    allowFontScaling={false}
+                                    style={{
+                                        color: focused ? '#ee125a' : '#91A2AD',
+                                        width: 50,
+                                        fontSize: 11,
+                                        textAlign: 'center'
+                                    }}>
+                                    Account
+                                </Text>
+                            </>
+                        )
+                    }}
+                />
+            </BottomTab.Navigator>
     )
 }
